0d84f986cf24918166e16b2ab4143fef84ab914d
Simon Stettler <simon.stettler@t2b.ch>
2021-10-27 09:54:56 +0200

    forget base box

---
+++ 2. API-M Microgateway/configure.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PROVISION="Configuring WSO2 API Manager "
+echo "**************************************************** Provisioning '"$PROVISION"' is starting..."
+
+NEWHOSTNAME=192.168.56.12
+# Changing HostName
+echo "Changing HostName t: " $NEWHOSTNAME
+XMLFILE=/opt/wso2/wso2am/repository/conf/carbon.xml
+sed -i 's:<HostName>.*</HostName>:<HostName>'$NEWHOSTNAME'</HostName>:' $XMLFILE
+sed -i 's:<MgtHostName>.*</MgtHostName>:<MgtHostName>'$NEWHOSTNAME'</MgtHostName>:' $XMLFILE
+grep HostName $XMLFILE
+XMLFILE=/opt/wso2/wso2am/repository/conf/api-manager.xml
+sed -i 's:<HostName>.*</HostName>:<HostName>'$NEWHOSTNAME'</HostName>:' $XMLFILE
+grep HostName $XMLFILE
--- BASE-BOX/vagrant-base-box.sh
+++
@@ -1,9 +0,0 @@
-BOX="centos-t2b"
-cd ~/VirtualBox\ VMs
-vagrant package --base=$BOX --output=$BOX.box
-
-mkdir -p ~/vagrant
-cp $BOX.box ~/vagrant
-cd ~/vagrant
-
-vagrant box add --name=$BOX $BOX.box
