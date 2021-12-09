6c810afc47dd8e17933834d937a6678e502c1753
luca <strozzi.luca@gmail.com>
2021-12-09 15:13:41 +0100

    Added "echo" commands

--- 1. API-M with Petstore-Pizzashack/t2bTraining.sh
+++ 1. API-M with Petstore-Pizzashack/t2bTraining.sh
@@ -1,12 +1,20 @@
+PROVISION="t2b Training for API Manager (no Micro-gateway)"
+echo "**************************************************** Provisioning '"$PROVISION"' is starting..."
+
+echo "Here are the current available environments:"
 apictl get envs
 
-# apictl add env production
-
-apictl add-env -e dev \
+echo "Executing: apictl add env dev"
+apictl add env dev \
 --registration https://localhost:9443 \
 --publisher https://localhost:9443 \
 --devportal https://localhost:9443 \
 --admin https://localhost:9443 \
 --token https://localhost:8243/token
 
+echo "Changing status of dev env"
 apictl change-status api -a Publish -n PizzsShack -v 1.0.0 -e dev
+
+
+
+echo "**************************************************** Provisioning '"$PROVISION"' is finished."
