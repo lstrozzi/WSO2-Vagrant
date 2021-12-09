d15a313a5bf3bd59797654f9a7adc6f69b400fcf
luca <strozzi.luca@gmail.com>
2021-12-09 15:13:19 +0100

    Added TestScript.sh

---
+++ 1. API-M with Petstore-Pizzashack/TestScript.sh
@@ -0,0 +1,6 @@
+PROVISION="TEST TEST TEST"
+echo "**************************************************** Provisioning '"$PROVISION"' is starting..."
+
+echo "I'm beautiful"
+
+echo "**************************************************** Provisioning '"$PROVISION"' is finished."
