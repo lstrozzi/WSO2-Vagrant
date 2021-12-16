7cdbfd6d5a360e6f2d4018000c15e24ff4875444
luca <strozzi.luca@gmail.com>
2021-12-16 11:20:38 +0100

    deleted bullshit example from internet

--- 99. Simple Backend/DeployBackend.sh
+++ 99. Simple Backend/DeployBackend.sh
@@ -1,6 +1,10 @@
 PROVISION="DEPLOY BACKEND"
 echo "**************************************************** Provisioning '"$PROVISION"' is starting..."
 
-echo "I'm beautiful"
+echo "* Copying source files of the Java Backend"
+cp -rf /vagrant/src ~/.
+
+echo "* Compiling Java Backend"
+cd ~/src/BackEnd
 
 echo "**************************************************** Provisioning '"$PROVISION"' is finished."
