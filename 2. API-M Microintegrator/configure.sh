42a67093dbbf588d02448aacf0dd6e257a0e2df1
luca <strozzi.luca@gmail.com>
2022-01-19 09:58:35 +0100

    Added comments Removed obsolete stuff

--- 2. API-M Microintegrator/configure.sh
+++ 2. API-M Microintegrator/configure.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-PROVISION="Configuring WSO2 API Manager "
+PROVISION="Configuring WSO2 Enterprise Integrator"
 echo "**************************************************** Provisioning '"$PROVISION"' is starting..."
 
-NEWHOSTNAME=192.168.56.13
-# Changing HostName
-echo "Changing HostName t: " $NEWHOSTNAME
-XMLFILE=/opt/wso2/wso2am/repository/conf/carbon.xml
-sed -i 's:<HostName>localhost</HostName>:<HostName>'$NEWHOSTNAME'</HostName>:' $XMLFILE
-sed -i 's:<MgtHostName>localhost</MgtHostName>:<MgtHostName>'$NEWHOSTNAME'</MgtHostName>:' $XMLFILE
-grep HostName $XMLFILE
-XMLFILE=/opt/wso2/wso2am/repository/conf/api-manager.xml
-sed -i 's:<HostName>.*</HostName>:<HostName>'$NEWHOSTNAME'</HostName>:' $XMLFILE
-grep HostName $XMLFILE
+echo "*** Nothing was done"
+
+echo "**************************************************** Provisioning '"$PROVISION"' is finished."
