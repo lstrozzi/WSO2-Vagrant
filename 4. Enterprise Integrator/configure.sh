afb2e3a46afc26eeb80e4715cbc983f6084c9e56
luca <strozzi.luca@gmail.com>
2022-01-19 10:06:51 +0100

    Added new VM

---
+++ 4. Enterprise Integrator/configure.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+PROVISION="Configuring WSO2 Enterprise Integrator"
+echo "**************************************************** Provisioning '"$PROVISION"' is starting..."
+
+echo "*** Nothing was done"
+
+echo "**************************************************** Provisioning '"$PROVISION"' is finished."
